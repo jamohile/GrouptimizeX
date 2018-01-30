ed9ef1faf4d50e07fd544b889acf9b66069b9ee4
Jay Mohile <jamohile@gmail.com>
2018-01-29 20:09:30 -0500

    modified code to make more universal in future

--- src/ca/mohile/Main.java
+++ src/ca/mohile/Main.java
@@ -1,20 +1,15 @@
 package ca.mohile;
 
-import java.io.*;
-import java.nio.file.FileSystem;
-import java.util.*;
-
-import jdk.nashorn.internal.ir.debug.JSONWriter;
 import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Iteration iteration = new Iteration();
-        iteration.configureFromJSONFile("sample2.json");
+        iteration.configureFromJSONFile("anj's.json");
         iteration.optimize(3, 2);
     }
 
@@ -22,41 +17,6 @@ public class Main {
     Function to read in a state from
     CMD.
      */
-    static void configureFromCMD() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many people?: ");
-        int numPeople = scanner.nextInt();
-        System.out.println("Okay, ready for " + numPeople + " people.");
-
-        /*
-        Get Names, create people.
-         */
-        for (int x = 1; x <= numPeople; x++) {
-            System.out.println("What is person " + x + "'s name?: ");
-            String name = scanner.next();
-            Person.add(new Person(name));
-        }
-        /*
-        get number of choices per person
-         */
-        System.out.println("How many choices per person?: ");
-        int numChoices = scanner.nextInt();
-
-        for (Person person : Person.people) {
-            for (int choice = 0; choice < numChoices; choice++) {
-                System.out.println("Choice " + (choice + 1) + " for " + person.name + ": ");
-                String choiceName;
-                do {
-                    choiceName = scanner.next();
-                } while (Person.getByName(choiceName) == null || choiceName.equals(person.name));
-                Person choicePerson = Person.getByName(choiceName);
-                person.addChoice(choicePerson, numChoices - choice);
-            }
-        }
-        Person.calculateLikabilities();
-        System.out.println("Awesome, grouptimize is ready to go.\n\n\n");
-
-    }
 }
 
 
